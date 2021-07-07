0b65e57efb5502650717ca4f8be77d056c6d1b49
fabientrd <fabientreand@gmail.com>
2021-07-07 17:35:29 +0200

    source

---
+++ level6/source.c
@@ -0,0 +1,16 @@
+void	n(){
+	system("/bin/cat /home/user/level7/.pass");
+	return ;
+}
+
+void	m(){
+	puts("Nope");
+	return ;
+}
+
+int		main(int ac, char **av){
+	char *s;
+
+	s = (char *)malloc(64);
+	return (0);
+}
